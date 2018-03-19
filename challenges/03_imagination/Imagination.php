73f85691cf49d8624674b77058f52d2a33411815
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2018-03-19 15:12:18 +0100

    Universalisation du code & Ajout fonctionnalité calculatrice
    + Nettoyage du code pour respecter la mise en page universelle
    + Gestion du cas de division par 0

--- challenges/03_imagination/Imagination.php
+++ challenges/03_imagination/Imagination.php
@@ -6,26 +6,26 @@ $NbCaractaffMaxParLignes = 1;
 $NbEspace = 25;
 $NbEspaceAff = 0;
 
-while ($NbCaractaffMaxParLignes < 10)                                           //Tant que l'on a pas afficher 10 lignes, on boucle
-{
-  while ($NbEspace > $NbEspaceAff)                                              //Tant que l'on a affiché un nb d'espace < à nb à afficher, on boucle
-  {
+while ( $NbCaractaffMaxParLignes < 10 ) {                                           //Tant que l'on a pas afficher 10 lignes, on boucle
+
+  while ( $NbEspace > $NbEspaceAff ) {                                              //Tant que l'on a affiché un nb d'espace < à nb à afficher, on boucle
     print ' ';                                                                  //On affiche un espace
     $NbEspaceAff++;                                                             //On incrémente de 1 le nb d'espaces affichés
   }
-  $NbEspace--;                                                                  //On réduit de 1 le nb d'espaces max pour la prochaine ligne
-$NbEspaceAff = 0;                                                               //On remet à 0 le nb d'espaces aff pour la prochaine ligne
 
-  while ($NbCaractaff < $NbCaractaffMaxParLignes)                               //Tant que le nb à afficher < au nombre de caractère pour cette ligne, on boucle
-  {
+  $NbEspace--;                                                                  //On réduit de 1 le nb d'espaces max pour la prochaine ligne
+  $NbEspaceAff = 0;                                                               //On remet à 0 le nb d'espaces aff pour la prochaine ligne
+
+  while ( $NbCaractaff < $NbCaractaffMaxParLignes ) {                               //Tant que le nb à afficher < au nombre de caractère pour cette ligne, on boucle
     print $NbAff3;                                                              //On affiche le nb
     $NbAff3++;                                                                  //On incrémente le nb
     $NbCaractaff++;
   }
+
   $NbAff3--;
   $NbAff3--;
-  while ($NbAff3 >= 1)                                                          //Tant que le nb à afficher < au nombre de caractère pour cette ligne, on boucle
-  {
+
+  while ( $NbAff3 >= 1 ) {                                                          //Tant que le nb à afficher < au nombre de caractère pour cette ligne, on boucle
     print $NbAff3;                                                              //On affiche le nb
     $NbAff3--;                                                                  //On incrémente le nb
   }
@@ -34,4 +34,5 @@ $NbEspaceAff = 0;                                                               
   $NbCaractaffMaxParLignes++;                                                   //On augmente le nb de catactères max par ligne
   $NbAff3 = 1;
   $NbCaractaff = 0;
+
 }
