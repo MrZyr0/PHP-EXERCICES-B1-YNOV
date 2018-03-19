a258fed4161258e376aef85e60bec64f41453a25
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2018-03-19 16:31:30 +0100

    Création tableau
    + Début de remplissage du tableau

---
+++ Exercices Cours/2 - Tableaux Associatifs.php
@@ -0,0 +1,17 @@
+<?php
+$TableauUtilisateurs = [
+    [
+        'Nom' => 'DUCHMOL',
+        'Prenom' => 'Jean',
+        'Age' => 32,
+        'Email' => 'jeand@fake.com',
+        'Poste' => 'Comptas'
+    ],
+    [
+        'Nom' => 'DUCHMOL',
+        'Prenom' => 'Jean',
+        'Age' => 32,
+        'Email' => 'jeand@fake.com',
+        'Poste' => 'Comptas'
+    ]
+];
