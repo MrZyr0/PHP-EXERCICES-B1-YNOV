4d320a5414729b6cb706bf68c77a3871e54a6471
MrZyr0 <32927775+MrZyr0@users.noreply.github.com>
2018-03-19 17:23:03 +0100

    Corrections exercice n°2
    + Modification de l'indentation pour respecter les standards
    + Correction de la variable aléatoire
    + Ajout des virgules à la fin des tableaux pour la bonne pratique

--- challenges/04_Generateur_de_Phrases/gen.php
+++ challenges/04_Generateur_de_Phrases/gen.php
@@ -1,23 +1,80 @@
 <?php
 
-$subject = ['Jean', 'Robert', 'Jacques', 'Henry', 'Adolf', 'Vladimir', 'Staline', 'Mandela', 'Obama', 'Piong Chang', 'Joris', 'Philipe'];
-$verb = ['mange', 'regarde', 'ouvre', 'découpe', 'change', 'veut', 'aime', 'vend', 'achete', 'découvre', 'tape', 'prend'];
-$complement = ['un haricot', 'un chien', 'un nain de jardin', 'une table', 'une tarte', 'un papier', 'des fleurs', 'des cacahuétes', 'le surimi', 'un yaourt', 'des pop-corn', 'du thon', 'une caisse', 'une valise', 'son ordinateur', 'sa tablette', ];
+print '***********************************' . "\n";
+print '*   Exercice de phrase aléatoire  *' . "\n";
+print 'de Matthieu Dabin & Julien Seixas *' . "\n";
+print '***********************************' . "\n\n\n";
+
+$subjects = [
+    'Jean',
+    'Robert',
+    'Jacques',
+    'Henry',
+    'Adolf',
+    'Vladimir',
+    'Staline',
+    'Mandela',
+    'Obama',
+    'Piong Chang',
+    'Joris',
+    'Philipe',
+];
+
+$verbs = [
+    'mange',
+    'regarde',
+    'ouvre',
+    'découpe',
+    'change',
+    'veut',
+    'aime',
+    'vend',
+    'achete',
+    'découvre',
+    'tape',
+    'prend',
+];
+
+$complements = [
+    'un haricot',
+    'un chien',
+    'un nain de jardin',
+    'une table',
+    'une tarte',
+    'un papier',
+    'des fleurs',
+    'des cacahuétes',
+    'le surimi',
+    'un yaourt',
+    'des pop-corn',
+    'du thon',
+    'une caisse',
+    'une valise',
+    'son ordinateur',
+    'sa tablette',
+];
+
 $choice = "oui";
+
+
 
 
  while ($choice === "oui")
  {
-    $rdmsujet = mt_rand(0, count($subject) - 1);
-    $rdmverbe = mt_rand(0, count($verb) - 1);
-    $rdmcomplement = mt_rand(0, count($complement) - 1);
+    $rdmsujet = mt_rand(0, count($subjects) - 1);
+    $rdmverbe = mt_rand(0, count($verbs) - 1);
+    $rdmcomplement = mt_rand(0, count($complements) - 1);
 
-    print $subject[$rdmsujet] . ' ' . $verb[$rdmverbe] . ' ' . $complement[$rdmcomplement] . "\n";
+    print $subjects[$rdmsujet] . ' ' . $verbs[$rdmverbe] . ' ' . $complements[$rdmcomplement] . "\n";
 
     $choice = readline("Relancer ? non | oui : ");
 
     if ( $choice != "oui" && $choice != "non" ) {
     $choice = "oui";
     }
+
+    print "\n\n\n";
+
  }
-print "\n";
+
+print "\n\n\n";
